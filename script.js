c36d6f0514e38407168b40de6425043ba689b637
Gustavo Warmling Kayser <64161534+GustavoKayser@users.noreply.github.com>
2022-04-02 19:33:07 +0000

    Botões adicionados

---
+++ script.js
@@ -0,0 +1,8 @@
+function fauna() {
+	document.getElementById("fauna").scrollIntoView({behavior: "smooth"});
+	
+}
+
+function flora() {
+	document.getElementById("flora").scrollIntoView({behavior: "smooth"});
+}
